cd143eceeb6731749ae7791cf7f06f7a64bd82ba
Valerii Bihun <valeriibihun.co@gmail.com>
2022-04-18 17:20:47 -0600

    deleting boxes

--- src/Box/Box.jsx
+++ src/Box/Box.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Box = (props) => {
-    let width = props.width
-    let height = props.height
-    let color = props.color
+  let width = props.width;
+  let height = props.height;
+  let color = props.color;
 
-    
   return (
-      <div>
-          <div style={{backgroundColor: `${color}`, width:`${width}px`, height:`${height}px`}}> </div>
-          <div>Width:{width} Height: {height} Color: {color}</div>
+    <div>
+      <div
+        style={{
+          backgroundColor: `${color}`,
+          width: `${width}px`,
+          height: `${height}px`,
+        }}
+      >
+        {" "}
       </div>
-    
-  )
-}
+      <div>
+        Width:{width} Height: {height} Color: {color}
+      </div>
+    </div>
+  );
+};
 
-export default Box
+export default Box;
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,97 +1,106 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Box from "../Box/Box";
 
 const Main = () => {
-    const [width, setWidth] = useState("");
-    const [color, setColor] = useState("");
-    const [height, setHeight] = useState("");
-    const [boxArray, setBoxArray] = useState([]);
+  const [width, setWidth] = useState("");
+  const [color, setColor] = useState("");
+  const [height, setHeight] = useState("");
+  const [boxArray, setBoxArray] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (width !== `` && height !== ``) {
-            let currentBox = { width: width, height: height, color: color};
-            setBoxArray([...boxArray, currentBox]);
-            setWidth({ value: "" });
-            setHeight({ value: "" });
-            setColor({ value: "" });
-          } else {
-            alert("Type in width and height please!");
-          }
-      };
-    
-      const handleChangeColor = (e) => {
-        setColor({ value: e.target.value });
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (width !== `` && height !== ``) {
+      let currentBox = { width: width, height: height, color: color };
+      setBoxArray([...boxArray, currentBox]);
+      setWidth({ value: "" });
+      setHeight({ value: "" });
+      setColor({ value: "" });
+    } else {
+      alert("Type in width and height please!");
+    }
+  };
 
-    const handleChangeWidth = (e) => {
-        setWidth({ value: e.target.value });
-      };
-    
-      const handleChangeHeight = (e) => {
-        setHeight({ value: e.target.value });
-      };
+  const handleChangeColor = (e) => {
+    setColor({ value: e.target.value });
+  };
+
+  const handleChangeWidth = (e) => {
+    setWidth({ value: e.target.value });
+  };
+
+  const handleChangeHeight = (e) => {
+    setHeight({ value: e.target.value });
+  };
+  
+  const deleteButton = (e, height, width, color) => {
+    e.preventDefault();
+    let result = boxArray.find(({height, width, color}) => height === height && width === width && color === color )
+    console.log(result)
+    setBoxArray(boxArray.filter(box => box !== result)) 
+  }
+
   return (
-  <div >
+    <div>
       <div className="border border-gray-800 m-6 p-8  bg-[#6982d5] text-white rounded-xl shadow-2xl shadow-slate-500">
-      <form className="mb-6 " onSubmit={(e) => handleSubmit(e)}>
-        <div className="pb-4 ">
-          <label className="mr-9 ">
-            Width:
+        <form className="mb-6 " onSubmit={(e) => handleSubmit(e)}>
+          <div className="pb-4 ">
+            <label className="mr-9 ">
+              Width:
+              <input
+                type="text"
+                name="width"
+                value={width.value}
+                onChange={(e) => handleChangeWidth(e)}
+                className="ml-1 border border-gray-800 bg-[#e5f8ea] text-black rounded-md"
+              />
+            </label>
+          </div>
+          <div className="pb-4 ">
+            <label className=" ">
+              Height:
+              <input
+                type="text"
+                name="height"
+                value={height.value}
+                onChange={(e) => handleChangeHeight(e)}
+                className="ml-1 border border-gray-800 bg-[#e5f8ea] text-black rounded-md "
+              />
+            </label>
+          </div>
+          <div className="pb-4 ">
+            <label className=" ">
+              Color:
+              <input
+                type="text"
+                name="height"
+                value={color.value}
+                onChange={(e) => handleChangeColor(e)}
+                className="ml-1 border border-gray-800 bg-[#e5f8ea] text-black rounded-md "
+              />
+            </label>
+          </div>
+          <div className="flex justify-center items-center ">
             <input
-              type="text"
-              name="width"
-              value={width.value}
-              onChange={(e) => handleChangeWidth(e)}
-              className="ml-1 border border-gray-800 bg-[#e5f8ea] text-black rounded-md"
+              type="submit"
+              value="Submit"
+              className="p-3 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 rounded-md  border border-gray-800 shadow-2xl shadow-slate-600"
             />
-          </label>
-        </div>
-        <div className="pb-4 ">
-          <label className=" ">
-          Height:
-            <input
-              type="text"
-              name="height"
-              value={height.value}
-              onChange={(e) => handleChangeHeight(e)}
-              className="ml-1 border border-gray-800 bg-[#e5f8ea] text-black rounded-md "
-            />
-          </label>
-        </div>
-        <div className="pb-4 ">
-          <label className=" ">
-          Color:
-            <input
-              type="text"
-              name="height"
-              value={color.value}
-              onChange={(e) => handleChangeColor(e)}
-              className="ml-1 border border-gray-800 bg-[#e5f8ea] text-black rounded-md "
-            />
-          </label>
-        </div>
-        <div className="flex justify-center items-center ">
-          <input
-            type="submit"
-            value="Submit"
-            className="p-3 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 rounded-md  border border-gray-800 shadow-2xl shadow-slate-600"
-          />
-        </div>
-      </form>
+          </div>
+        </form>
       </div>
-      
+
       <div>
         {boxArray[0]
           ? boxArray.map((box) => {
               return (
-                <div key={'hey'} className='flex justify-center items-center'> 
+                <div key={"hey"} className="flex justify-center items-center">
                   <Box
                     // use new library for uuid for each key!
                     height={box.height.value}
                     width={box.width.value}
                     color={box.color.value}
                   />
+                  <button className="ml-6 mt-2 p-1 bg-[#9c0726]" onClick={(e)=>deleteButton(e, box.height.value, box.width.value, box.color.value)}>X</button>
                 </div>
               );
             })
